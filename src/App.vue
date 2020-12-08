9895be82393c56e1fd2c9953b13b79a57c1fc8b9
xuwei <1098404803@qq.com>
2020-12-08 09:47:25 +0800

    feat: vite工具搭建项目

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <a-button type="primary"> Primary </a-button>
+</template>
+
+<script>
+import { Button } from 'ant-desigin-vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './index.css'
+// import ElementPlus from 'element-plus'
+// import 'element-plus/lib/theme-chalk/index.css'
+
+import Antd from 'ant-design-vue'
+// import 'ant-design-vue/dist/antd.css'
+
+createApp(App).use(Antd).mount('#app')
